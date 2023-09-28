9bbc7e947c551a9cb8c5cbee5e41f51cf079020c
Merlinovitch <a.merlin92@gmail.com>
2023-09-28 12:02:47 +0200

    exo5

--- script.js
+++
@@ -1,9 +0,0 @@
-const choixCouleur  = document.getElementById('choix-couleur')
-const bouton = document.getElementById('bouton')
-const champsTexte = document.getElementById('champsTexte')
-
-function ajouterTexte() {
-    champsTexte.value = choixCouleur.value
-}
-
-bouton.addEventListener('click' , ajouterTexte)
