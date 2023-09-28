81d967394b889d42457c8625825c78e49c34d3c0
Merlinovitch <a.merlin92@gmail.com>
2023-09-28 13:44:31 +0200

    exo5

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const ChampsTexte = document.getElementById('champsTexte')
 const bouton = document.getElementById('bouton')
 
+function compterCaracteres () {
+   const ChampsTexte = document.getElementById('champsTexte').value
+   const nbCaracteres = ChampsTexte.length
+   const nouvelleDiv = document.createElement('div')
+   const body = document.body
+   nouvelleDiv.textContent = 'le nombre de caractères est de ' + nbCaracteres
+   body.appendChild(nouvelleDiv)
+   
+}
 
-bouton.addEventListener('click', console.log(ChampsTexte.length))
+bouton.addEventListener('click', compterCaracteres)
