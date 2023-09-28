83a2d47671d12d40fd705a605bacc7dd1c15cde6
Merlinovitch <a.merlin92@gmail.com>
2023-09-28 11:14:55 +0200

    Exo4

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const liste = document.getElementById('liste')
-const searchBar = document.getElementById('searchBar')
-const mot = liste.getElementsByTagName('li')
-const button = document.getElementById('button')
+const choixCouleur  = document.getElementById('choix-couleur')
 
-function search() {
-
-    const rechercheText = searchBar.value
-
-
-    for (let i = 0; i < mot.length; i++) {
-        const item = mot[i].textContent
-        if (item === rechercheText) {
-            
-            mot[i].style.backgroundColor = "yellow"
-
-        } else {
-            mot[i].style.backgroundColor = ""
-        }        
-    }
-}
-
-button.addEventListener('click', search)
+console.log(choixCouleur)
